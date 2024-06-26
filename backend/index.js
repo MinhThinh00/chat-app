27f241d55b0b9d10b44e814306a97f76c8d688ff
Minh Thịnh <111744097+MinhThinh00@users.noreply.github.com>
2024-06-26 19:46:03 +0700

    edit cors

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,11 @@ dotenv.config();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173', // Thay đổi thành miền của ứng dụng của bạn
+    origin: 'https://chatapppclient.vercel.app', // Thay đổi thành miền của ứng dụng của bạn
     credentials: true // Cho phép gửi cookie
 }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://chatapppclient.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
